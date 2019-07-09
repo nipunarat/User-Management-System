b1b72afcd9d8cb38db6629595ef5d8b3c3e9bb6c
nipuna <nipunarat1997@gmail.com>
2019-07-09 20:57:51 +0530

    completed the connection file

--- inc/connection.php
+++ inc/connection.php
@@ -4,4 +4,12 @@
     $dbpass = '';
     $dbname = 'userdb';
 
+    $connection = mysqli_connect('localhost' , 'root' , '' , 'userdb');
+
+    //cheaking the connection
+
+    if(mysqli_connect_errno())
+    {
+        die('Database connection failed' . mysqli_connect_error());
+    }
 ?>
