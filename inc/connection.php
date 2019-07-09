4c6c558fed9fcdedcf9c2602ad58f1a8e78e18f9
nipuna <nipunarat1997@gmail.com>
2019-07-09 20:53:56 +0530

    created required variables for connection

--- inc/connection.php
+++ inc/connection.php
@@ -1,15 +1,7 @@
 <?php
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>connection</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = 'userdb';
+
 ?>
